63309dac09fef5cee41b6011808892c53787b6b6
Andini Faramita <faramitaandini@gmail.com>
2025-05-09 13:00:03 +0200

    Finished window container

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import {
     container,
+    topBar,
     heading,
+    windowDecor,
     navLinks,
     navLinkItem,
     navLinkText,
@@ -21,7 +24,36 @@ const Layout = ({ pageTitle, children }) => {
         `)
   return (
     <div className={container}>
-        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        <div className={topBar}>
+            <StaticImage
+                alt="Grass block icon from Minecraft" 
+                src="../images/Grass_Block.png"       
+                placeholder="blurred"
+                width={20}
+                height={20}
+                quality={100}
+            />
+
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+
+            <div className={windowDecor}>
+                <StaticImage
+                    alt="Minimize window icon"
+                    src="../images/minimize.png"
+                    width={12}
+                />
+                <StaticImage
+                    alt="Maximize window icon"
+                    src="../images/maximize.png"
+                    width={12}
+                />
+                <StaticImage
+                    alt="Close window icon"
+                    src="../images/close.png"
+                    width={12}
+                />             
+            </div>
+        </div>
         <nav>
             <ul className={navLinks}>
                 <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
@@ -33,7 +65,9 @@ const Layout = ({ pageTitle, children }) => {
             <h1 className={heading}>{pageTitle}</h1>
             {children}
         </main>
-        </div>
+
+        <div></div>
+    </div>
   )
 }
 
