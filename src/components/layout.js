2faae7deb1c1c6b915df685344936d5f8911d2f2
Andini Faramita <faramitaandini@gmail.com>
2025-05-11 22:28:36 +0200

    fixed bottom nav bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,20 @@
 import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import {
+    parent,
     container,
     topBar,
-    heading,
     windowDecor,
-    navLinks,
-    navLinkItem,
-    navLinkText,
-    siteTitle
+    // heading,
+    // navLinks,
+    // navLinkItem,
+    // navLinkText,
+    siteTitle,
+    main,
+    content,
+    bottomBar
+    // navBar
   } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
@@ -23,51 +28,70 @@ const Layout = ({ pageTitle, children }) => {
         }
         `)
   return (
-    <div className={container}>
-        <div className={topBar}>
-            <StaticImage
-                alt="Grass block icon from Minecraft" 
-                src="../images/Grass_Block.png"       
-                placeholder="blurred"
-                width={20}
-                height={20}
-                quality={100}
-            />
+    <div className={parent}>
+        <div className={container}>
 
-            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-
-            <div className={windowDecor}>
+            {/* Top window bar */}
+            <div className={topBar}>
                 <StaticImage
-                    alt="Minimize window icon"
-                    src="../images/minimize.png"
-                    width={12}
+                    alt="Grass block icon from Minecraft" 
+                    src="../images/grass_block.png"       
+                    placeholder="blurred"
+                    width={20}
+                    height={20}
+                    quality={100}
                 />
-                <StaticImage
-                    alt="Maximize window icon"
-                    src="../images/maximize.png"
-                    width={12}
-                />
-                <StaticImage
-                    alt="Close window icon"
-                    src="../images/close.png"
-                    width={12}
-                />             
+                <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+                <div className={windowDecor}>
+                    <StaticImage
+                        alt="Minimize window icon"
+                        src="../images/minimize.png"
+                        width={12}
+                    />
+                    <StaticImage
+                        alt="Maximize window icon"
+                        src="../images/maximize.png"
+                        width={12}
+                    />
+                    <StaticImage
+                        alt="Close window icon"
+                        src="../images/close.png"
+                        width={12}
+                    />             
+                </div>
             </div>
-        </div>
-        <nav>
-            <ul className={navLinks}>
-                <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-                <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-                <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
-            </ul>
-        </nav>
-        <main>
-            <h1 className={heading}>{pageTitle}</h1>
-            {children}
-        </main>
 
-        <div></div>
+            {/* Page content */}
+            {/* <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+                </ul>
+            </nav>
+            <main>
+                <h1 className={heading}>{pageTitle}</h1>
+                {children}
+            </main> */}
+            <div className={main}>
+                Main
+                <div className={content}>Content</div>
+
+                {/* Bottom navigation bar */}
+                <div className={bottomBar}>
+                    <StaticImage
+                        alt="Close window icon"
+                        src="../images/minecraft_hotbar.png"
+                        width={400}
+                        quality={100}
+                    />
+                    <header>bottom bar</header>
+                </div>
+            </div>
+
+        </div>
     </div>
+
   )
 }
 
